7fa27981ea47fb5a7ce9bb17e800eadd5cef8896
CGH <972186435@qq.com>
2018-12-13 20:56:46 +0800

    2222

---
+++ php/zhuce.php
@@ -0,0 +1,19 @@
+<?php header('Content-Type: text/html; charset=utf-8')?>
+<?php
+	require('db.php');
+	$user=$_POST['user'];
+	$pwd=$_POST['pwd'];	
+	$search = "SELECT `username` FROM user WHERE username='$user'";
+//	echo $search;
+	//获取记录集
+	$res = $conn -> query($search);
+//	echo $res -> num_rows;
+	if ($res -> num_rows > 0) {
+		echo $res -> num_rows;
+	}else{
+		$sql="insert into user(username,userpass) values ('$user','$pwd')";
+		echo $res -> num_rows;
+		$conn->query($sql);
+	}
+		$conn->close();
+?>	
